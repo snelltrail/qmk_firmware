3c83dc3d62f5694ed6cd2f9b1385cde0798970e9
Daniel Snell <dsnell2009@gmail.com>
2019-06-19 17:56:48 +1200

    Working tap dance example

--- keyboards/rama/m6_b/keymaps/snelltrail/keymap.c
+++ keyboards/rama/m6_b/keymaps/snelltrail/keymap.c
@@ -9,7 +9,12 @@ enum custom_keycodes {
   LATEX_CLEAN,
   LATEX_DEBUG,
   LATEX_CONT,
-  CTRL_C
+  CTRL_C,
+  EMAIL
+};
+
+enum {
+  TD_ONE_TWO = 1
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -56,17 +61,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // when key released
       }
       break;
+    case EMAIL:
+      if(record->event.pressed) {
+        SEND_STRING("dsnell2009@gmail.com");
+      } else {
+        // when key released
+      }
+      break;
   }
   return true;
 }
 
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_ONE_TWO]  = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_2)
+// Other declarations would go here, separated by commas, if you have them
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT(
-        LATEXMK, LATEX_TIDY, LATEX_CLEAN, LATEX_DEBUG, CTRL_C , OSL(1)),
+        LATEXMK, LATEX_TIDY, LATEX_CLEAN, 
+        LATEX_DEBUG, CTRL_C, TG(1)),
 
     [FIRST] = LAYOUT(
-        RESET, LATEX_CONT, KC_B, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT),
+        RESET, LATEX_CONT, EMAIL, 
+        TD(TD_ONE_TWO), KC_TRANSPARENT, TG(1)),
 
     LAYOUT(
         KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
