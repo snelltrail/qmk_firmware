0ccf7b600c909935fbe3de9f9017933cbaa0aaca
Daniel Snell <dsnell2009@gmail.com>
2019-11-06 22:43:08 +1300

    Prime_E keymap v1.1

--- keyboards/primekb/prime_e/keymaps/snelltrail/keymap.c
+++ keyboards/primekb/prime_e/keymaps/snelltrail/keymap.c
@@ -18,21 +18,21 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
 		KC_TAB,   KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,          KC_Y,   KC_U,     KC_I, KC_O,    KC_P,    KC_LBRC, KC_RBRC,
-		KC_LCTL,   KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_SCLN, KC_ENT,
-		KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,          MO(3),  KC_N,     KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
-		MO(2),  KC_LGUI,                    KC_LALT, LT(1, KC_SPC),	          KC_BSPC, KC_RALT,                          KC_RGUI, MO(2)
+		KC_LCTL,   KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_SCLN, LT(2, KC_ENT),
+		KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,          LT(3, KC_B),  KC_N,     KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
+		KC_LCTL,  KC_LGUI,                    KC_LALT, LT(1, KC_SPC),	          KC_BSPC, LM(1, MOD_LALT),    LM(1, MOD_LALT | MOD_LSFT), MO(2)
     ),
 
     LAYOUT(
-		KC_ESC,  KC_1,      KC_2,      KC_3,      KC_4,       KC_5,             KC_7,      KC_8,     KC_9,    KC_MINS,    KC_EQL,      KC_BSLS,  KC_GRV,
+		KC_ESC,  LALT(KC_1),      LALT(KC_2),      LALT(KC_3),      LALT(KC_4),       LALT(KC_5),             KC_7,      KC_8,     KC_9,    KC_MINS,    KC_EQL,      KC_BSLS,  KC_GRV,
 		KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_4,   KC_5,    KC_6,   KC_TRNS,   KC_LBRC,   KC_QUOT,
 		KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_0,   KC_1,    KC_2,   KC_3,   KC_TRNS,   KC_TRNS,  KC_TRNS,
 		KC_TRNS,  KC_TRNS,                         KC_TRNS,    KC_TRNS,	         KC_PENT,   KC_PDOT,                               KC_TRNS,  KC_TRNS
     ),
 
     LAYOUT(
-		KC_GRV,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_TRNS,    KC_UP,     KC_TRNS,   KC_UP,   KC_BSLS,   KC_GRV,
-		KC_CAPS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_LEFT,    KC_DOWN,   KC_LEFT,  KC_RIGHT,   KC_BSLS,
+		KC_CAPS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_TRNS,    KC_UP,     KC_TRNS,   KC_UP,   KC_BSLS,   KC_GRV,
+		KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_LEFT,    KC_DOWN,   KC_LEFT,  KC_RIGHT,   KC_BSLS,
 		KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,          KC_TRNS,   KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_DOWN,   KC_TRNS,   KC_TRNS,
 		BL_TOGG,   BL_STEP,                         KC_TRNS,    KC_TRNS,	        KC_TRNS,   KC_TRNS,                                     KC_TRNS,   KC_TRNS
     ),
